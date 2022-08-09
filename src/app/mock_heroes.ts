0138aec38735b1ffd47daa1f614246f212895d7b
tbfPrime <trevorjoins@gmail.com>
2022-08-09 21:28:04 +0530

    Added Hero list

---
+++ src/app/mock_heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  {id: 10, name: "Hero 10"},
+  {id: 20, name: "Hero 20"},
+  {id: 30, name: "Hero 30"},
+  {id: 40, name: "Hero 40"},
+  {id: 50, name: "Hero 50"},
+  {id: 60, name: "Hero 60"},
+  {id: 70, name: "Hero 70"},
+  {id: 80, name: "Hero 80"},
+  {id: 90, name: "Hero 90"}
+];
