5ec087fd5b766ff1eafca8aca6428225c8d8ff36
tbfPrime <trevorjoins@gmail.com>
2022-08-16 21:52:37 +0530

    Added requisites for HTTP request.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from "./in-memory-data.service";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +23,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService{
+
+  constructor() { }
+  createDb() {
+    const heroes = [
+      { id: 12, name: 'Hero 12' },
+      { id: 13, name: 'Hero 13' },
+      { id: 14, name: 'Hero 14' },
+      { id: 15, name: 'Hero 15' },
+      { id: 16, name: 'Hero 16' },
+      { id: 17, name: 'Hero 17' },
+      { id: 18, name: 'Hero 18' },
+      { id: 19, name: 'Hero 19' }
+    ];
+
+    return {heroes};
+  }
+}
